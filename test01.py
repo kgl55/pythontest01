b84d3ad8f816986efc865c6129a4eef7195a060f
chensheng <kgl55@163.com>
2017-12-27 11:37:00 +0800

    test

---
+++ test01.py
@@ -0,0 +1,14 @@
+
+
+#!/usr/bin/python3
+
+import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+
+
+counter = 100          # 整型变量
+miles   = 1000.0       # 浮点型变量
+name    = "runoob"     # 字符串
+
+print (counter)
+print (miles)
+print (name)
